dec8272876f85ebfbdd26e35adfd291b74c66258
João Pedro <joaopedrobastossantana@gmail.com>
2025-12-02 10:07:04 -0300

    ajuste erro no botão acessar projeto

--- script.js
+++ script.js
@@ -122,33 +122,38 @@ $(document).ready(function () {
     if (projectselected == "Destino dos Sonhos") {
       projectdescription.html(`Criei esse projeto para ele ser uma Landing Page para uma agência de viagens, foi muito gratificante fazer esse projeto pois pude treinar muito CSS e como estilizar uma página com ele.`);
       usedproject.html(`HTML <br> CSS <br> Jquery`)
+      acessProject.css("display", 'flex');
     } else if (projectselected == "Weather App") {
       projectdescription.html(`Neste projeto utilizei o Vite para criar um app em React a idéia do projeto e mostrar a previsão do tempo atual e a dos próximos 5 dias com base na cidade que você pesquisar. <br><br> Utilizei o Axios para consumir a API do OpenWeather para buscar as informações sobre o clima também utilizei a API da Unsplash que gera as imagens de fundo da tela ele também utiliza a cidade que você utilizou como parametro para buscar a próxima imagem de fundo.`);
       usedproject.html(`Unsplash API <br> OpenWeather API <br> React <br> HTML <br> CSS <br> Javascript`)
       acessProject.attr('href', 'https://weatherapp-stack.netlify.app/');
+      acessProject.css("display", 'flex');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/WeatherApp')
       videoProject.attr('src', 'https://drive.google.com/file/d/1mgbCgYMikjFQmWCo9gHCFDz2waNtgKBb/preview')
     } else if (projectselected == "Jogo da Memória") {
       projectdescription.html(`Este projeto foi o fechamento do meu curso técnico em informática, neste projeto criei um jogo da memória utilizando C# com o windows form ele foi desenvolvido no Visual Studio 2022, mesmo dando trabalho e consumindo muito tempo foi muito gratificante e divertido fazer ele foi um projeto trabalhoso e que fiz do jeito que queria, fiquei muito feliz com o resultado.`);
       usedproject.html(`C# <br> .Net Framework <br> Windows Form`)
-      acessProject.css("display" , 'none');
+      acessProject.css("display", 'none');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/Jogo_Da_Memoria')
       videoProject.attr('src', 'https://drive.google.com/file/d/1Q2WFwfq1V-N5AbbD3yEvJwIQJcnemSI9/preview')
-    }else if (projectselected == "Burgos House") {
+    } else if (projectselected == "Burgos House") {
       projectdescription.html(`Visando criar algo como um e-commerce esse foi meu primeiro feito para uma hamburgueria fícticia, contém carrinho e os pedidos chegam via whatsapp`);
       usedproject.html(`HTML <br> CSS <br> JS <br>JQUERY`);
+      acessProject.css("display", 'flex');
       acessProject.attr('href', 'https://burgoshouse.netlify.app/');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/BurgosHouse')
       videoProject.attr('src', 'https://drive.google.com/file/d/1T0DVQDQZQ-UCJhoq_yNdse1L9STyPsKC/preview')
     } else if (projectselected == "Movies Lib") {
       projectdescription.html(`Projeto feito para pesquisar sobre filmes, nele você pode efetuar a pesquisa ver a sinopse e todas as informações como faturamento, orçamento, duração etc... `);
       usedproject.html(`HTML <br> CSS <br> JS <br>React <br> Vite <br> TMDB API`);
+      acessProject.css("display", 'flex');
       acessProject.attr('href', 'https://joaopflix.netlify.app/');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/movies_lib')
       videoProject.attr('src', 'https://drive.google.com/file/d/1pN9dmBdKFvEBXezzPhX7eu6rwXUjANOo/preview')
     } else if (projectselected == "To Do") {
       projectdescription.html(`Este projeto foi feito para me aprimorar em Typescript, funciona como uma lista de afazeres para organizar o seu dia a dia`);
       usedproject.html(`HTML <br> CSS <br> Typescript <br>React`);
+      acessProject.css("display", 'flex');
       acessProject.attr('href', 'https://to-do-reacttypescript.netlify.app/');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/To-Do-Typescript')
       videoProject.attr('src', 'https://drive.google.com/file/d/1SFVLV4N_3xGmseft3WYskWjcxgwgB4k6/preview')
@@ -156,13 +161,15 @@ $(document).ready(function () {
       projectdescription.html(`Este Projeto foi criado como um clone do instagram e foi utilizada várias tecnologias para a sua criação e hospedagem, como Cloudinary e Render
         ele possui várias funcionalidades, Registro, Login, Like e comentários, postagem, edição e deleção de postagens uma api que foi bem construída com meios de autenticação e token para algumas rotas feito com JWT`);
       usedproject.html(`HTML <br> CSS <br> Javascript <br>React<br> NodeJs <br> MongoDB <br> Express`);
+      acessProject.css("display", 'flex');
       acessProject.attr('href', 'https://reactgraam.netlify.app/');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/ReactGram')
       videoProject.attr('src', 'https://drive.google.com/file/d/1Hsht_fK-bR_pWHl27oanme1khhx9Ndq8/preview')
-    } 
+    }
     else {
       projectdescription.html(`Este projeto foi criado com a funcionalidade de recomendar filmes com base de outro filme que você goste, o usuário coloca o nome do filme que gosta e o app faz a busca com esse filme na API e busca filmes parecidos com o relatado.`);
       usedproject.html(`Tmdb API <br> React <br> HTML <br> CSS <br> Javascript`)
+      acessProject.css("display", 'flex');
       acessProject.attr('href', 'https://cinemmatch.netlify.app/');
       acessRepo.attr('href', 'https://github.com/joaopedro-stack/CineMatch')
       videoProject.attr('src', 'https://drive.google.com/file/d/1HovzHJxd0tGjwa1SrqqwYHKR-qw2BVsK/preview')
